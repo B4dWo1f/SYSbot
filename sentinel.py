0b306cc1eb2ee9181019770359095e212278bf45
Noel <B4dWo1f@protonmail.com>
2022-03-21 19:54:53 +0100

    added suspend command

--- sentinel.py
+++ sentinel.py
@@ -129,6 +129,7 @@ if __name__ == '__main__':
    #sys handlers
    dpt.add_handler(CH('lock', cb.screen_lock))
    dpt.add_handler(CH('unlock', cb.screen_unlock, Filters.chat(CR.ADMINS_id)))
+   dpt.add_handler(CH('suspend', cb.suspend))
 
    #sentinel handlers
    dpt.add_handler(CH('screenshot',cb.screenshot, pass_job_queue=True))
